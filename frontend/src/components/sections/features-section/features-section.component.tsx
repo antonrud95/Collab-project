5dbfc46709d70da02b89a653d56f691255cd81a5
arud95 <antonrud95@yandex.ru>
2020-10-27 13:47:41 +0200

    feature: add slider

--- frontend/src/components/sections/features-section/features-section.component.tsx
+++ frontend/src/components/sections/features-section/features-section.component.tsx
@@ -1,14 +1,80 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import { graphql, useStaticQuery } from 'gatsby'
 
+import SimpleSlider from '~/components/ui/features-carousel/features-carousel.component'
+import SliderItem from '~/components/ui/features-carousel-item/features-carousel-item.component'
 import styles from './features-section.module.scss'
 
 const FeaturesSection = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      imageCamera: file(
+        relativePath: { eq: "images/features/features-camera.png" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 71) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      imageCalendar: file(
+        relativePath: { eq: "images/features/features-calendar.png" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 62) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
   return (
     <React.Fragment>
       <div className={styles.featuresContainer}>
         <Container>
           <div className={styles.borderOrnament} />
+          <SimpleSlider>
+            <SliderItem
+              image={data.imageCamera.childImageSharp.fluid}
+              itemtitle={'Import Photos & Videos'}
+              itemtext={
+                'Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.'
+              }
+              itemdescription={'Find out more'}
+              itemTitleStyles={styles.sliderItemTitle}
+              itemTextStyles={styles.sliderItemText}
+              itemDescStyles={styles.sliderItemDesc}
+              imgWrapper={styles.sliderImageWrapperCamera}
+              textWrapper={styles.sliderItemTextWrapper}
+            />
+            <SliderItem
+              image={data.imageCalendar.childImageSharp.fluid}
+              itemtitle={'Calendar API Built-In'}
+              itemtext={
+                'Kickstart your design process for a wide variety of apps such as calendar, analytics, virtual assistant, file manager.'
+              }
+              itemdescription={'Find out more'}
+              itemTitleStyles={styles.sliderItemTitle}
+              itemTextStyles={styles.sliderItemText}
+              itemDescStyles={styles.sliderItemDesc}
+              imgWrapper={styles.sliderImageWrapperCalendar}
+              textWrapper={styles.sliderItemTextWrapper}
+            />
+            <SliderItem
+              image={data.imageCalendar.childImageSharp.fluid}
+              itemtitle={'Calendar API Built-In'}
+              itemtext={
+                'Kickstart your design process for a wide variety of apps such as calendar, analytics, virtual assistant, file manager.'
+              }
+              itemdescription={'Find out more'}
+              itemTitleStyles={styles.sliderItemTitle}
+              itemTextStyles={styles.sliderItemText}
+              itemDescStyles={styles.sliderItemDesc}
+              imgWrapper={styles.sliderImageWrapperCalendar}
+              textWrapper={styles.sliderItemTextWrapper}
+            />
+          </SimpleSlider>
         </Container>
         <div className={styles.spearOrnament} />
       </div>
---
+++ frontend/src/components/ui/features-carousel/features-carousel.component.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import FC from '~/types/fc'
+
+interface Props {
+  children: ReactNode
+}
+
+const SimpleSlider: FC<Props> = ({ children }) => {
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+  return <Slider {...settings}>{children}</Slider>
+}
+
+export default SimpleSlider
