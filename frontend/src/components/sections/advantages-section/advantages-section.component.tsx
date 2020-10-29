696041b7e87e187afd1771eb4b2ecb3b4fff7bc5
arud95 <antonrud95@yandex.ru>
2020-10-29 10:26:58 +0200

    wip: work with advantages section icons

--- frontend/src/components/sections/advantages-section/advantages-section.component.tsx
+++ frontend/src/components/sections/advantages-section/advantages-section.component.tsx
@@ -1,11 +1,45 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import SButton from '~/components/ui/general/button/button.component'
 import SInput from '~/components/ui/general/input/input.component'
+
+import { graphql, useStaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
 
 import styles from './advantages-section.module.scss'
 
 const AdvantagesSection = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      imageFirst: file(
+        relativePath: { eq: "icons/advantages/icon-first.png" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 56) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      imageSecond: file(
+        relativePath: { eq: "icons/advantages/icon-second.png" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 56) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      imageThird: file(
+        relativePath: { eq: "icons/advantages/icon-third.png" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 56) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
   return (
     <div className={styles.advantagesOuter}>
       <Container>
@@ -20,6 +54,43 @@ const AdvantagesSection = () => {
             </SButton>
           </div>
         </div>
+      </Container>
+      <Container className={styles.advantagesContent}>
+        <Row>
+          <Col xs="12" md="6">
+            <h3 className={styles.advantagesTitle}>Auto Adaptive layouts</h3>
+            <p className={styles.advantagesText}>Responsive{<br />}DNA.</p>
+            <p className={styles.advantagesDescription}>
+              Easily create truly responsive components while deploying
+              production-ready code.
+            </p>
+            <div className={styles.iconWrapper}>
+              <div className={styles.iconStylesWrapper}>
+                <Img fluid={data.imageFirst.childImageSharp.fluid} />
+              </div>
+              <p className={styles.iconWrapperText}>Advanced pinning</p>
+            </div>
+            <div className={styles.iconWrapper}>
+              <div className={styles.iconStylesWrapper}>
+                <Img fluid={data.imageFirst.childImageSharp.fluid} />
+              </div>
+              <p className={styles.iconWrapperText}>
+                Relative positioning with containers
+              </p>
+            </div>
+            <div className={styles.iconWrapper}>
+              <div className={styles.iconStylesWrapper}>
+                <Img fluid={data.imageFirst.childImageSharp.fluid} />
+              </div>
+              <p className={styles.iconWrapperText}>
+                Percentage-based geometry
+              </p>
+            </div>
+          </Col>
+          <Col xs="12" md="6">
+            Hello
+          </Col>
+        </Row>
       </Container>
     </div>
   )
